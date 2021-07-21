4a865641cf2c6e8ebb749d60bdd6bea1d5ced943
efajunk <efa.junk@gmail.com>
2021-07-21 13:24:24 +0300

    feature to add many groups to folder added

--- job.py
+++ job.py
@@ -1,14 +1,20 @@
 # initiate func, that will make a rows of scripts for each folder
 def script_constructor(list_of_paths, filename, group_asking=None):
     for path in list_of_paths:
-        group_asking = input(f'Какую группу использовать для {path}(домен по умолчанию oasiscatalog): ')
-        ps_get_acl = f'$acl = Get-Acl {path};'
-        ps_access_rule = f'$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("oasiscatalog\{group_asking}", "ReadAndExecute", "Allow");'
-        ps_set_access_rule = '$acl.SetAccessRule($AccessRule);'
-        ps_set_acl = f'$acl | Set-Acl {path};'
-        rows_list = [ps_get_acl, ps_access_rule, ps_set_access_rule, ps_set_acl]
-        for row in rows_list:
-            write_row(filename, row)
+        while True:
+            group_asking = input(f'Group to add access for: {path}(домен по умолчанию oasiscatalog): ')
+            # if group_asking == 'ag':
+            #     print([i for i in get_access(filename, path)])
+            ps_get_acl = f'$acl = Get-Acl {path};'
+            ps_access_rule = f'$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("oasiscatalog\{group_asking}", "ReadAndExecute", "Allow");'
+            ps_set_access_rule = '$acl.SetAccessRule($AccessRule);'
+            ps_set_acl = f'$acl | Set-Acl {path};'
+            rows_list = [ps_get_acl, ps_access_rule, ps_set_access_rule, ps_set_acl]
+            for row in rows_list:
+                write_row(filename, row)
+            if input(f'Is there other groups to add?(enter to skip): '):
+                continue
+            break
     return
 
 # Getting paths to folders from acl_data file and pass to script_constructor
@@ -21,6 +27,18 @@ def get_path(filename):
                 index_slice_start_from = i.rfind(":")
                 list_of_paths.append(i[index_slice_start_from - 1:].strip())
         return list_of_paths
+
+# getting actual access to folder
+# def get_access(filename, path):
+#     with open(filename, "r", encoding="utf-16") as file:
+#         index_slice_start_from = 0
+#         list_of_access = []
+#         for i in file.readlines():
+#             if path in i:
+#                 while 'Audit' not in i:
+#                     index_slice_start_from = i.rfind(":")
+#                     list_of_access.append(i[index_slice_start_from - 1:].strip())
+#         return list_of_access
 
 def write_row(filename, row):
     with open(filename, "a", encoding="utf-16") as file:
